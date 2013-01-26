c598c77b838a8e84be9a683e104fbf8ac55d3cd5
Andrew Waterman <waterman@cs.berkeley.edu>
2013-01-25 19:02:58 -0800

    update to library-based fesvr; assume $RISCV env var

--- build-nogcc.sh
+++ build-nogcc.sh
@@ -5,11 +5,16 @@
 
 . build.common
 
+if [ ! `which riscv-gcc` ]
+then
+  echo "riscv-gcc doesn't appear to be installed; use the full-on build.sh"
+  exit 1
+fi
+
 echo "Starting RISC-V Toolchain build process"
 
 build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
-build_gcc
 build_project riscv-pk --prefix=$RISCV/target --host=riscv
 
 echo -e "\\nRISC-V Toolchain installation completed!"
