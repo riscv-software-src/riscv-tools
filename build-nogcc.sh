ace0d9ec2e6ff5b28610bddb1be7dc566b9483c4
Andrew Waterman <waterman@cs.berkeley.edu>
2013-04-29 22:00:55 -0700

    use new gcc configure script; build isa tests

--- build-nogcc.sh
+++ build-nogcc.sh
@@ -15,6 +15,5 @@ echo "Starting RISC-V Toolchain build process"
 
 build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
-build_project riscv-pk --prefix=$RISCV/target --host=riscv
 
 echo -e "\\nRISC-V Toolchain installation completed!"
