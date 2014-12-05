2991201a5263ee9e88ca380d53383cf67e3eae59
Andrew Waterman <waterman@cs.berkeley.edu>
2014-12-04 16:04:49 -0800

    Use correct --host= setting for pk
    This fixes the Darwin build.

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo "Starting RISC-V Toolchain build process"
 build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
 build_project riscv-gnu-toolchain --prefix=$RISCV
-CC=riscv64-unknown-elf-gcc build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --host=riscv
+build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --host=riscv64-unknown-elf
 # ignore translation files from qemu
 #cd riscv-qemu; git update-index --assume-unchanged po/*; cd ..
 #build_project riscv-qemu --prefix=$RISCV --target-list=riscv-softmmu
