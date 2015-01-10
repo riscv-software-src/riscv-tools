cd6b88abac28e844feb575561d4f96446df7ead5
Sagar Karandikar <karandikarsagar@gmail.com>
2015-01-10 15:45:46 -0800

    build qemu

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
 build_project riscv-gnu-toolchain --prefix=$RISCV
 build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --host=riscv64-unknown-elf
 # ignore translation files from qemu
-#cd riscv-qemu; git update-index --assume-unchanged po/*; cd ..
-#build_project riscv-qemu --prefix=$RISCV --target-list=riscv-softmmu
+cd riscv-qemu; git update-index --assume-unchanged po/*; cd ..
+build_project riscv-qemu --prefix=$RISCV --target-list=riscv-softmmu
 build_tests
 
 echo -e "\\nRISC-V Toolchain installation completed!"
