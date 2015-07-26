14451aa1aac08727605a05290d996d6b721f4856
Wei Song <wsong83@gmail.com>
2015-07-26 11:32:21 +0100

    prepare to set up env for lowrisc untethered soc

--- build.sh
+++ build.sh
@@ -10,10 +10,6 @@ echo "Starting RISC-V Toolchain build process"
 build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
 build_project riscv-gnu-toolchain --prefix=$RISCV
-build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --host=riscv64-unknown-elf
-# ignore translation files from qemu
-cd riscv-qemu; git update-index --assume-unchanged po/*; cd ..
-build_project riscv-qemu --prefix=$RISCV --target-list=riscv-softmmu
 build_tests
 
 echo -e "\\nRISC-V Toolchain installation completed!"
