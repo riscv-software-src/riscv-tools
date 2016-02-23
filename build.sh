c5bad086cc5d01a32baf227b071e300e1d7ce3b8
Palmer Dabbelt <palmer@dabbelt.com>
2016-02-22 21:09:18 -0800

    Install the tests along with the other tools
    This allows Travis to cache the built tests more easily, and we're already
    doing this at the BWRC (where the tool builds were flaky).

--- build.sh
+++ build.sh
@@ -13,4 +13,7 @@ build_project riscv-gnu-toolchain --prefix=$RISCV
 CC= CXX= build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --host=riscv64-unknown-elf
 build_tests
 
+mkdir -p $RISCV/riscv64-unknown-elf/share/riscv-tests
+cp -a riscv-tests/{isa,benchmarks} $RISCV/riscv64-unknown-elf/share/riscv-tests
+
 echo -e "\\nRISC-V Toolchain installation completed!"
