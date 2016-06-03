5e56c867722e1480fc4f6884e7c6101e54956c75
Wei Song <wsong83@gmail.com>
2016-06-03 11:52:04 +0100

    do not build tests to save docker space

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
 build_project riscv-gnu-toolchain --prefix=$RISCV
 CC= CXX= build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --host=riscv64-unknown-elf
-build_tests
+
+#build_tests
 
 echo -e "\\nRISC-V Toolchain installation completed!"
