cd7f2ac7cbf850c3ca74c34e6262335d112da599
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2016-07-12 11:18:52 -0700

    Add a "--with-xlen" argument
    This should help users who are trying to build coherent 32-bit software
    ecosystems.
    
    The pk bump is to work around a compiler bug.

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 # Script to build RISC-V ISA simulator, proxy kernel, and GNU toolchain.
 # Tools will be installed to $RISCV.
 
-. build.common
+. build.common "$@"
 
-echo "Starting RISC-V Toolchain build process"
+echo "Starting RISC-V Toolchain build process, XLEN=$XLEN"
 
 build_project riscv-fesvr --prefix=$RISCV
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
-build_project riscv-gnu-toolchain --prefix=$RISCV
-CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv64-unknown-elf
-build_project riscv-tests --prefix=$RISCV/riscv64-unknown-elf
+build_project riscv-gnu-toolchain --prefix=$RISCV --with-xlen=${XLEN}
+CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv${XLEN}-unknown-elf
+build_project riscv-tests --prefix=$RISCV/riscv${XLEN}-unknown-elf
 
 echo -e "\\nRISC-V Toolchain installation completed!"
