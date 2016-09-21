424ee2ef47feaa51b61dc7a34f5a533ef40aab14
Andrew Waterman <waterman@cs.berkeley.edu>
2016-09-21 11:01:34 -0700

    Provide an RV32IMA build script, rather than RV32IM
    The old script was actually building an RV32IMA toolchain and an RV32IM
    spike.  This reconciles the difference, and then allows building riscv-pk.

--- build-rv32ima.sh
+++ build-rv32ima.sh
@@ -8,7 +8,8 @@
 echo "Starting RISC-V Toolchain build process"
 
 build_project riscv-fesvr --prefix=$RISCV
-build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV --with-isa=RV32IM
+build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV --with-isa=RV32IMA
 build_project riscv-gnu-toolchain --prefix=$RISCV --with-arch=RV32IMA
+CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv32-unknown-elf
 
 echo -e "\\nRISC-V Toolchain installation completed!"
