598b3b1d5cf548cd84c04e995b9c5a76a37929f9
Bryton Hall <email@bryton.io>
2017-10-04 04:14:50 -0500

    trim README and get rid of extra ENV variable
    This will build the tools in $RISCV/build; the idea being that $RISCV
    will contain all of the various projects (similiar to the submodules of
    this repo).
    
    Also add support for appending configure flags to build.sh tools.

--- build.sh
+++ build.sh
@@ -4,46 +4,99 @@
 # Tools will be installed to $RISCV.
 
 usage () {
-    echo "usage: $0 [rv32isa] || [<tool>] [<tool>] ..."
-    echo ""
+    echo
+    echo "usage: $0 [<tool> --<flag> --<flag> ...] [<tool>] ..."
+    echo
     echo "where <tool> can be any one of:"
     echo "    openocd fesvr isa-sim gnu-toolchain pk tests"
-    echo ""
-    echo "[default]      build all of the tools"
+    echo "and the --reset flag"
+    echo "    clears all flags of the preceeding tool (except --prefix)"
+    echo
+    echo "[no args]      build all of the default tools"
     echo "rv32ima        build 32-bit versions"
+    echo
+    echo "The default flags are:"
+    echo
+    echo "riscv-openocd$openocdflags"
+    echo "riscv-fesvr$fesvrflags"
+    echo "riscv-isa-sim$isaflags"
+    echo "riscv-gnu-toolchain$gnuflags"
+    echo "riscv-pk$pkflags"
+    echo "riscv-tests$testsflags"
+    echo
+    echo "with --prefix=\$RISCV assumed where missing"
+    echo
     exit 1
 }
 
-if [ $# -eq 0 ]
-then
+if [ $# -eq 0 ]; then
     all=true
 fi
 
-while test $# -gt 0
-do
+# default flags
+openocdflags=" --enable-remote-bitbang --enable-jtag_vpi --disable-werror"
+fesvrflags=" "
+isaflags=" --with-fesvr=$RISCV"
+gnuflags=" "
+pkflags=" --host=riscv64-unknown-elf"
+testsflags=" "
+
+appendarg () {
+    eval new="\$$1"
+    append="$new $2"
+
+    if [ "$2" = "--reset" ]
+    then
+        eval $1= ""
+    else
+        eval $1=\$append
+    fi
+}
+
+while test $# -gt 0; do
     case "$1" in
-        openocd)       openocd=true ;;
-        fesvr)         fesvr=true  ;;
-        isa-sim)       isa=true  ;;
-        gnu-toolchain) gnu=true ;;
-        pk)            pk=true  ;;
-        tests)         tests=true ;;
-        rv32ima)       rv32ima=true ;;
-        -h)            usage ;;
-        *) echo "error: unrecognized parameter: $1"
-           usage ;;
+        openocd)       openocd=true
+                       lastarg="$1" ;;
+        fesvr)         fesvr=true
+                       lastarg="$1" ;;
+        isa-sim)       isa=true
+                       lastarg="$1" ;;
+        gnu-toolchain) gnu=true
+                       lastarg="$1" ;;
+        pk)            pk=true
+                       lastarg="$1" ;;
+        tests)         tests=true
+                       lastarg="$1" ;;
+        rv32ima)       rv32ima=true
+                       lastarg="$1" ;;
+        --*)
+            appendarg "${lastarg}flags" "$1"
+            if [ $rv32ima ]; then
+            usage
+            fi ;;
+        -h) 
+            usage ;;
+        *)  
+            echo "error: unrecognized parameter: $1"
+            usage ;;
     esac
     shift
 done
 
-if [ $rv32ima ] && [ $all ] || [ $opencd ] || [ $fesvr ] || [ $isa ] || [ $gnu ] || [ $pk ] || [ $tests ]
-then
+if [ $rv32ima ] && [ $all ] || [ $opencd ] || [ $fesvr ] || [ $isa ] || [ $gnu ] || [ $pk ] || [ $tests ]; then
    echo "error: rv32ima cannot be used with any other option"
    usage
 fi
 
-if [ "x$RISCV" = "x" ]
-then
+if [ $rv32ima ]; then
+    openocdflags=" --enable-remote-bitbang --disable-werror"
+    isaflags=" --with-fesvr=$RISCV --with-isa=rv32ima"
+    gnuflags=" --with-arch=rv32ima --with-abi=ilp32"
+    pkflags=" --host=riscv32-unknown-elf"
+fi
+
+
+if [ "x$RISCV" = "x" ]; then
   echo "Please set the RISCV environment variable to your preferred install path."
   exit 1
 fi
@@ -56,23 +109,24 @@ PATH="$RISCV/bin:$PATH"
 
 set -e
 
-function build_project {
+build_project () {
   PROJECT="$1"
   shift
   echo
-  if [ -e "$PROJECT/build" ]
-  then
+
+  if [ -e "$PROJECT/build" ]; then
     echo "Removing existing $PROJECT/build directory"
     rm -rf "$PROJECT/build"
   fi
-  if [ ! -e "$PROJECT/configure" ]
-  then
+
+  if [ ! -e "$PROJECT/configure" ]; then
     (
       cd "$PROJECT"
       find . -iname configure.ac | sed s/configure.ac/m4/ | xargs mkdir -p
       autoreconf -i
     )
   fi
+
   mkdir -p "$PROJECT/build"
   cd "$PROJECT/build"
   echo `git rev-parse HEAD` > build.version
@@ -88,50 +142,28 @@ function build_project {
 
 echo "Starting RISC-V Toolchain build process"
 
-if [ $all ] || [ $openocd ]
-then
-    build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --enable-jtag_vpi --disable-werror
+if [ $all ] || [ $openocd ]; then
+    build_project riscv-openocd --prefix=$RISCV/build "$openocdflags"
 fi
 
-if [ $all ] || [ $fesvr ]
-then
-    build_project riscv-fesvr --prefix=$RISCV
+if [ $all ] || [ $fesvr ]; then
+    build_project riscv-fesvr --prefix=$RISCV/build "$fesvrflags"
 fi
 
-if [ $all ] || [ $isa ] || [ $rv32ima ]
-then
-    if [ $rv32ima ]
-    then
-        build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV --with-isa=rv32ima
-    else
-        build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
-    fi
+if [ $all ] || [ $isa ] || [ $rv32ima ]; then
+    build_project riscv-isa-sim --prefix=$RISCV/build "$isaflags"
 fi
 
-if [ $all ] || [ $gnu ] || [ $rv32ima ]
-then
-    if [ $rv32ima ]
-    then
-        build_project riscv-gnu-toolchain --prefix=$RISCV --with-arch=rv32ima --with-abi=ilp32
-    else
-        build_project riscv-gnu-toolchain --prefix=$RISCV
-    fi
+if [ $all ] || [ $gnu ] || [ $rv32ima ]; then
+    build_project riscv-gnu-toolchain --prefix=$RISCV/build "$gnuflags"
 fi
 
-if [ $all ] || [ $pk ] || [ $rv32ima ]
-then
-    if [ $rv32ima ]
-    then
-        CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv32-unknown-elf
-        build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --disable-werror
-    else
-        CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv64-unknown-elf
-    fi
+if [ $all ] || [ $pk ] || [ $rv32ima ]; then
+    CC= CXX= build_project riscv-pk --prefix=$RISCV/build "$pkflags"
 fi
 
-if [ $all ] || [ $tests ]
-then
-    build_project riscv-tests --prefix=$RISCV/riscv64-unknown-elf
+if [ $all ] || [ $tests ]; then
+    build_project riscv-tests --prefix=$RISCV/build/riscv64-unknown-elf "$testsflags"
 fi
 
 echo -e "\\nRISC-V Toolchain installation complete!"
