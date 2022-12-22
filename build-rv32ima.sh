37d771de1a4a6a4982356edb8a3bfdcc310d2f40
Zenithal <i@zenithal.me>
2022-12-22 16:23:09 +0800

    Bump everything
    Similar to https://github.com/chipsalliance/rocket-tools/pull/23

--- build-rv32ima.sh
+++ build-rv32ima.sh
@@ -9,6 +9,7 @@ echo "Starting RISC-V Toolchain build process"
 
 build_project riscv-isa-sim --prefix=$RISCV --with-isa=rv32ima
 CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv32-unknown-elf
-build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --disable-werror
+(cd riscv-openocd && ./bootstrap)
+build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --enable-jtag_vpi --disable-werror --disable-jlink
 
 echo -e "\\nRISC-V Toolchain installation completed!"
